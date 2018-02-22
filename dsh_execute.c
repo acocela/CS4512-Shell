914e4744bfc9fe8854a87b2207d55c04899b5397
David <Schorcher@users.noreply.github.com>
2018-02-22 17:47:35 -0500

    Update dsh_execute.c

--- dsh_execute.c
+++ dsh_execute.c
@@ -7,7 +7,7 @@ int dsh_execute(char **args)
     return 1;
   }
 
-  for (i = 0; i < lsh_num_builtins(); i++) {
+  for (i = 0; i < dsh_num_builtins(); i++) {
     if (strcmp(args[0], builtin_str[i]) == 0) {
       return (*builtin_func[i])(args);
     }
